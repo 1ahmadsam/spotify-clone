72e40f23d3832564a48e36ed3cd535b402b4064f
1ahmadsam <samiahmad360@hotmail.com>
2020-09-25 05:31:58 -0400

    adding project to github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebAPI from 'spotify-web-api-js';
+import Player from './Player';
+import { useStateValue } from './StateProvider';
+const spotify = new SpotifyWebAPI();
 
 function App() {
+  const [{ user, token }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // Set token
+    const hash = getTokenFromUrl();
+    window.location.hash = '';
+    let _token = hash.access_token;
+
+    if (_token) {
+      spotify.setAccessToken(_token);
+
+      dispatch({
+        type: 'SET_TOKEN',
+        token: _token,
+      });
+
+      spotify.getPlaylist('6dCvdNaCR7qj7f2rkkwQDj').then((response) =>
+        dispatch({
+          type: 'SET_DISCOVER_WEEKLY',
+          discover_weekly: response,
+        })
+      );
+
+      spotify.getMyTopArtists().then((response) =>
+        dispatch({
+          type: 'SET_TOP_ARTISTS',
+          top_artists: response,
+        })
+      );
+
+      dispatch({
+        type: 'SET_SPOTIFY',
+        spotify: spotify,
+      });
+
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: 'SET_USER',
+          user,
+        });
+      });
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: 'SET_PLAYLISTS',
+          playlists,
+        });
+      });
+    }
+  }, [token, dispatch]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      {token ? <Player spotify={spotify} /> : <Login />}
     </div>
   );
 }
---
+++ src/reducer.js
@@ -0,0 +1,69 @@
+export const initialState = {
+  user: null,
+  playlists: [],
+  spotify: null,
+  discover_weekly: null,
+  top_artists: null,
+  playing: false,
+  item: null,
+  //remove after developing
+  //   token:
+  //     'BQCK1s7k_TXL_DKsJstmIghjdhVJ14iNh6qGfiQgoQ5bjjZcnW6j2zlUhrFE1VQFCLhRNQMUsZe_wjJWhmDnhxtpBO0hvW60q7YjyRpmcdb1wSR11c25LTP90rM5hcxAXj_1L0zhQaG3OtkBR6VMn9s-KqkWyYA',
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case 'SET_USER':
+      return {
+        ...state,
+        user: action.user,
+      };
+
+    case 'SET_PLAYING':
+      return {
+        ...state,
+        playing: action.playing,
+      };
+
+    case 'SET_ITEM':
+      return {
+        ...state,
+        item: action.item,
+      };
+
+    case 'SET_DISCOVER_WEEKLY':
+      return {
+        ...state,
+        discover_weekly: action.discover_weekly,
+      };
+
+    case 'SET_TOP_ARTISTS':
+      return {
+        ...state,
+        top_artists: action.top_artists,
+      };
+
+    case 'SET_TOKEN':
+      return {
+        ...state,
+        token: action.token,
+      };
+
+    case 'SET_SPOTIFY':
+      return {
+        ...state,
+        spotify: action.spotify,
+      };
+
+    case 'SET_PLAYLISTS':
+      return {
+        ...state,
+        playlists: action.playlists,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
